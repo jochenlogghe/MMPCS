a05dad9a4cf9190c4706681f339ba68168298a0c
jochenl <jochen.logghe@picanol.be>
2015-09-25 13:46:06 +0000

    heroku vars

--- config/config.js
+++ config/config.js
@@ -1,12 +1,11 @@
 module.exports = function(app){
     var config      = {};
     
-    config.secret = 'thisismylittlesecret';
-    config.tokenExpireDays = 1;
-    
     switch(app.get('env')){
         case 'dev':
-            config.dbUrl =  'mongodb://teccomcrmuser:teccomcrmuser@ds027483.mongolab.com:27483/teccomcrm';
+            config.dbUrl =  process.env.MONGOURLDEV;
+            config.secret = process.env.SECRETDEV;
+            config.tokenExpireDays = process.env.TOKENEXPIRESDEV;
             break;
     }
     
